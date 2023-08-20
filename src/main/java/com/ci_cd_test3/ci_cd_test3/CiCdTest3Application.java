78de770cab832381ba98dc52a5b4db857ed7356c
mbisethomas <lemasawe@gmail.com>
2023-08-20 20:48:04 +0300

    449 commit

---
+++ src/main/java/com/ci_cd_test3/ci_cd_test3/CiCdTest3Application.java
@@ -0,0 +1,13 @@
+package com.ci_cd_test3.ci_cd_test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTest3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci_cd_test3/ci_cd_test3/CiCdTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci_cd_test3.ci_cd_test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
